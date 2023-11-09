2f14fea73122404a6d0bf4c4a62f7f40793aa324
Render78 <mariojuliantor@hotmail.com>
2023-11-09 15:08:47 -0300

    Filtro por orden alfabetico de productos

--- assets/scripts/productos.js
+++ assets/scripts/productos.js
@@ -1,15 +1,12 @@
 import { products } from "./data/products-data.js";
 
-// Obtén el contenedor donde deseas insertar las tarjetas
 const productListDiv = document.getElementById('productList');
 
-// Recorre los productos y crea una tarjeta para cada uno
 products.forEach(product => {
-    // Crea un div para cada producto
+
     const productDiv = document.createElement('div');
     productDiv.classList.add('product-card');
 
-    // Inserta la información del producto en el div
     productDiv.innerHTML = `
         <img src="${product.image}" alt="${product.title}">
         <h3>${product.title}</h3>
@@ -17,7 +14,37 @@ products.forEach(product => {
         <p>${product.description}</p>
         <button>Añadir al carrito</button>
     `;
-
-    // Agrega el div del producto al contenedor de lista de productos
     productListDiv.appendChild(productDiv);
+});
+
+let ordenAscendente = true;
+
+document.getElementById('btnOrdenar').addEventListener('click', () => {
+    ordenAscendente = !ordenAscendente;
+
+    products.sort((a, b) => {
+        if (ordenAscendente) {
+            return a.title.localeCompare(b.title);
+        } else {
+            return b.title.localeCompare(a.title);
+        }
+    });
+
+    const productListDiv = document.getElementById('productList');
+    productListDiv.innerHTML = '';
+
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product-card');
+
+        productDiv.innerHTML = `
+            <img src="${product.image}" alt="${product.title}">
+            <h3>${product.title}</h3>
+            <p>Precio: $${product.price}</p>
+            <p>${product.description}</p>
+            <button>Añadir al carrito</button>
+        `;
+
+        productListDiv.appendChild(productDiv);
+    });
 });

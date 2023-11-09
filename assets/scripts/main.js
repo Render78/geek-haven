405586862d91acdd2cda40b551e158ad39d317d4
Render78 <mariojuliantor@hotmail.com>
2023-11-09 14:46:36 -0300

    darkMode inyectado en los HTML

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,30 +1,3 @@
-let toggle = document.getElementById('toggle');
-let lblToggle = document.getElementById('lblToggle');
-let modeIcon = document.getElementById('modeIcon');
+import { darkMode } from "./darkMode.js";
 
-if (localStorage.getItem('darkMode') === 'activated') {
-    document.body.classList.add('dark');
-    lblToggle.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
-    toggle.checked = true;
-}
-
-toggle.addEventListener('change', (event) => {
-    let checked = event.target.checked;
-    document.body.classList.toggle('dark');
-
-    if (checked === true) {
-        lblToggle.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
-        localStorage.setItem('darkMode', 'activated');
-    } else {
-        lblToggle.innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
-        localStorage.removeItem('darkMode');
-    }
-});
-
-modeIcon.addEventListener('click', () => {
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark');
-        lblToggle.innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
-        localStorage.removeItem('darkMode');
-    }
-});
+darkMode();

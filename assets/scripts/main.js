3584aaed883d7b9b6ab3dcd87a8af61ce3cfb2e0
Render78 <mariojuliantor@hotmail.com>
2023-11-25 13:08:47 -0300

    productos cargados en el index con fetch

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,37 +1,47 @@
-import { products } from "./data/products-data.js";
-
 function renderProductsOnSale(productArray) {
     const productsOnSaleDiv = document.getElementById('productsOnSale');
     productsOnSaleDiv.innerHTML = '';
 
-    const productsOnSale = productArray.filter(product => product.sale);
+    productArray.forEach(product => {
+        if (product.sale) {
+            const cardDiv = document.createElement('div');
+            cardDiv.classList.add('col-md-4', 'mb-3');
 
-    productsOnSale.forEach(product => {
-        const cardDiv = document.createElement('div');
-        cardDiv.classList.add('col-md-4', 'mb-3');
-
-        cardDiv.innerHTML = `
-            <div class="card">
-                <img src="${product.image}" class="card-img-top" alt="${product.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${product.title}</h5>
-                    <p class="card-text">Precio: $${product.price}</p>
-                    <p class="card-text">${product.description}</p>
-                    <p class="card-text"><small class="text-muted">¡En oferta!</small></p>
-                    <button class="btn btn-primary btn-add-to-cart" data-product='${JSON.stringify(product)}'>Añadir al carrito</button>
+            cardDiv.innerHTML = `
+                <div class="card">
+                    <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">Precio: $${product.price}</p>
+                        <p class="card-text">${product.description}</p>
+                        <p class="card-text"><small class="text-muted">¡En oferta!</small></p>
+                        <button class="btn btn-primary btn-add-to-cart" data-product='${JSON.stringify(product)}'>Añadir al carrito</button>
+                    </div>
                 </div>
-            </div>
-        `;
+            `;
 
-        productsOnSaleDiv.appendChild(cardDiv);
+            productsOnSaleDiv.appendChild(cardDiv);
 
-        const addToCartBtn = cardDiv.querySelector('.btn-add-to-cart');
-        addToCartBtn.addEventListener('click', () => {
-            const productData = JSON.parse(addToCartBtn.getAttribute('data-product'));
-            agregarAlCarritoDesdeIndex(productData);
-        });
+            const addToCartBtn = cardDiv.querySelector('.btn-add-to-cart');
+            addToCartBtn.addEventListener('click', () => {
+                const productData = JSON.parse(addToCartBtn.getAttribute('data-product'));
+                agregarAlCarritoDesdeIndex(productData);
+            });
+        }
     });
 }
+
+async function fetchProductsOnSale() {
+    try {
+        const response = await fetch('../assets/scripts/data/productos.json');
+        const products = await response.json();
+        renderProductsOnSale(products);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
+
+fetchProductsOnSale();
 
 function agregarAlCarritoDesdeIndex(producto) {
     let productosEnOferta = JSON.parse(localStorage.getItem('productosEnOferta')) || [];
